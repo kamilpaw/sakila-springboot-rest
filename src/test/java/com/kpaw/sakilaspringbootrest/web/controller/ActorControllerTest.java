2a079cae9066e529154f1e6ca30e9647ad647e2d
Kamil Pawłowski <pawlowskikamil.mail@gmail.com>
2022-02-22 07:23:47 +0100

    code cleanup

---
+++ src/test/java/com/kpaw/sakilaspringbootrest/web/controller/ActorControllerTest.java
@@ -0,0 +1,36 @@
+package com.kpaw.sakilaspringbootrest.web.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActorControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findActor() {
+    }
+
+    @Test
+    void saveNewActor() {
+    }
+
+    @Test
+    void updateActor() {
+    }
+
+    @Test
+    void deleteActor() {
+    }
+
+    @Test
+    void findActorsByFilmId() {
+    }
+
+    @Test
+    void searchActorsByFirstNameAndLastName() {
+    }
+}
